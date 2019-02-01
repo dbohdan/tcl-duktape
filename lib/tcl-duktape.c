854e4db864cdbc30b6039b18167c5ee15a2b0169
dbohdan <dbohdan@dbohdan.com>
2019-02-01 22:58:21 +0200

    docs: update copyright

--- lib/tcl-duktape.c
+++ lib/tcl-duktape.c
@@ -1,6 +1,6 @@
 /*
  * Tcl bindings for Duktape.
- * Copyright (c) 2015, 2016, 2017
+ * Copyright (c) 2015, 2016, 2017, 2018, 2019
  * dbohdan and contributors listed in AUTHORS
  * This code is released under the terms of the MIT license. See the file
  * LICENSE for details.
